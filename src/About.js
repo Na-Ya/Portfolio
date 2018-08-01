f34770256d6bbde0100b0fd6bd732fea3e179294
Nabil Yafai <nabilyafai90@gmail.com>
2018-08-01 10:04:13 -0400

    began fleshing out about section

---
+++ src/About.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import profile from './images/profile.png';
+const About = () => {
+	return (
+		<div id="about-parent">
+			<div>
+				<img src={profile} />
+			</div>
+			<div>
+				<p>
+					Hot chicken air plant poutine, cliche mumblecore gentrify chillwave.
+					Health goth ethical hexagon salvia freegan mlkshk normcore meggings
+					raw denim enamel pin messenger bag squid literally. Bushwick aesthetic
+					master cleanse lomo, coloring book forage before they sold out vegan
+					swag craft beer butcher brooklyn flannel pug. Wolf freegan pinterest
+					subway tile tacos tofu. Tofu tilde twee photo booth readymade ramps
+					thundercats ennui iPhone raw denim lumbersexual chambray flannel
+					occupy paleo. Offal kitsch palo santo, asymmetrical chillwave
+					taxidermy lo-fi snackwave vexillologist narwhal kombucha.{' '}
+				</p>
+			</div>
+		</div>
+	);
+};
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Navbar } from './directory';
+import image from './images/profile.png';
+import { Navbar, About } from './directory';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
 				<Navbar />
+				<About />
 			</div>
 		);
 	}
