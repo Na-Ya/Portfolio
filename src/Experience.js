a878f5fcfed2b7b9a07674b2d13a723c035e76e1
Nabil Yafai <nabilyafai90@gmail.com>
2018-08-03 17:22:32 -0400

    removed materual ui since its not used. Updated card layout for work exp to make it more responsive using pure css

--- src/Experience.js
+++ src/Experience.js
@@ -8,15 +8,16 @@ const Experience = () => {
 			<h4 style={{ marginTop: 0, color: 'black' }}>Tap for details</h4>
 			{experiences.map((experience, index) => {
 				return (
-					<div key={index} className="scene">
-						<div
-							className="card"
-							onClick={e => {
-								e.preventDefault();
-								e.currentTarget.classList.toggle('is-flipped');
-							}}
-						>
-							<div className="job-parent card__face card__face--front">
+					<div
+						key={index}
+						className="flip-container"
+						onClick={e => {
+							e.preventDefault();
+							e.currentTarget.classList.toggle('flip');
+						}}
+					>
+						<div className="flipper">
+							<div className="job-parent front">
 								<div className="job-icon-parent">
 									<img
 										id="job-icon"
@@ -33,20 +34,20 @@ const Experience = () => {
 									</p>
 								</div>
 							</div>
-							<div className="job-parent card__face card__face--back">
-								<div className="job-icon-parent">
-									<img
-										id="job-icon"
-										src="https://vignette.wikia.nocookie.net/powerlisting/images/1/18/Yin-Yang.png/revision/latest?cb=20121028014051"
-									/>
-								</div>
-								<div className="job-info-parent">
+							<div className="job-parent back">
+								<div className="job-info-parent-back">
 									<h3 id="job-title">This is a job title</h3>
-									<p>Company Name</p>
-									<p>11-11-1111 - 11-11-1111</p>
-									<p>
-										BackBackBackBackBackBackBackBackBackBackBackBackBackBackBackBackBackBackBack{' '}
-									</p>
+									<ul>
+										<li>Keytar bitters slow-carb authentic cliche.</li>{' '}
+										<li>
+											Fam brunch meh, gluten-free vaporware etsy pok pok green
+											juice retro craft beer.
+										</li>
+										<li>
+											Unicorn ennui leggings skateboard pinterest. Mlkshk cred
+											synth actually.
+										</li>
+									</ul>
 								</div>
 							</div>
 						</div>
