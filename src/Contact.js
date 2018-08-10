42eef65de5ea88cb25e45470a0c596e929b1276f
Nabil Yafai <nabilyafai90@gmail.com>
2018-08-10 12:12:57 -0400

    added personal contact links

--- src/Contact.js
+++ src/Contact.js
@@ -7,9 +7,28 @@ const Contact = () => {
 			<h1>Contact</h1>
 
 			<div id="contact-icons-parent">
-				<i className="fa fa-github fa-4x" aria-hidden="true" />
-				<i className="fa fa-linkedin-square fa-4x" aria-hidden="true" />
-				<i className="fa fa-envelope fa-4x" aria-hidden="true" />
+				<a
+					className="icon-link"
+					href="https://github.com/na-ya"
+					target="_blank"
+				>
+					{' '}
+					<i className="fa fa-github fa-4x" aria-hidden="true" />{' '}
+				</a>
+				<a
+					href="https://www.linkedin.com/in/nabil-yafai/"
+					className="icon-link"
+					target="_blank"
+				>
+					<i className="fa fa-linkedin-square fa-4x" aria-hidden="true" />
+				</a>
+				<a
+					href="mailto:nabilyafai90@gmail.com"
+					className="icon-link"
+					target="_blank"
+				>
+					<i className="fa fa-envelope fa-4x" aria-hidden="true" />{' '}
+				</a>
 			</div>
 		</div>
 	);
