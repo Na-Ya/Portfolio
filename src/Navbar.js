3156132fc3cafc3d54d1f1454d2fac98c8ee85b8
Nabil Yafai <nabilyafai90@gmail.com>
2018-08-10 16:21:13 -0400

    scroll spy working better now, more responsive to the sticky nav

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
+import Scrollspy from 'react-scrollspy';
 
 const NavBar = () => {
+	const offset = document.documentElement.clientHeight * 0.2;
 	return (
 		<header>
 			<div id="nav-home">
@@ -9,7 +11,7 @@ const NavBar = () => {
 			</div>
 			<div id="nav-links">
 				<nav>
-					<ul>
+					{/* <ul>
 						<li className="nav-link">
 							<AnchorLink href="#projects-parent">Projects</AnchorLink>
 						</li>
@@ -19,7 +21,28 @@ const NavBar = () => {
 						<li className="nav-link">
 							<AnchorLink href="#contact-parent">Contact</AnchorLink>
 						</li>
-					</ul>
+					</ul> */}
+					<Scrollspy
+						items={['projects-parent', 'experience-parent', 'contact-parent']}
+						currentClassName="is-current"
+						offset={-offset}
+					>
+						<li className="nav-link">
+							<AnchorLink href="#projects-parent" offset={offset}>
+								Projects
+							</AnchorLink>
+						</li>
+						<li className="nav-link">
+							<AnchorLink href="#experience-parent" offset={offset}>
+								Experience
+							</AnchorLink>
+						</li>
+						<li className="nav-link">
+							<AnchorLink href="#contact-parent" offset={offset}>
+								Contact
+							</AnchorLink>
+						</li>
+					</Scrollspy>
 				</nav>
 			</div>
 		</header>
