e0f59638e73bd03b94ddf537ab26f263a624f4b6
Nabil Yafai <nabilyafai90@gmail.com>
2018-08-16 17:43:23 -0400

    updated offset in navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,25 @@ import AnchorLink from 'react-anchor-link-smooth-scroll';
 import Scrollspy from 'react-scrollspy';
 
 const NavBar = () => {
-	const offset = document.documentElement.clientHeight * 0.2;
+	const offsetHeader = document.documentElement.clientHeight * 0.1;
+	const offset = document.documentElement.clientHeight * 0.1;
+	console.log('scroll', document.documentElement.scrollTop);
+
+	window.addEventListener('scroll', function(e) {
+		if (window.scrollY > offsetHeader) {
+			let navlinks = document.getElementById('nav-links');
+			console.log(navlinks.offsetHeight);
+
+			navlinks.classList.add('newfixed');
+		} else if (window.scrollY < offsetHeader) {
+			let navlinks = document.getElementById('nav-links');
+			navlinks.classList.remove('newfixed');
+		}
+	});
+	// if (document.documentElement.scrollTop > offset) {
+	// 	let navlinks = document.getElementById('nav-links');
+	// 	navlinks.classList.add('newfixed');
+	// }
 	return (
 		<header>
 			<div id="nav-home">
