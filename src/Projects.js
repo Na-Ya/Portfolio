ef203df780e477e09ec68f66a2c651544901a86e
Nabil Yafai <nabilyafai90@gmail.com>
2018-08-17 14:50:28 -0400

    made projects section more responsive desktop

--- src/Projects.js
+++ src/Projects.js
@@ -9,14 +9,16 @@ const Projects = () => {
 				return (
 					<div key={index}>
 						<h2>Project Title </h2>
-						<p>
-							Lorem ipsum dolor amet truffaut mustache viral, vexillologist
-							PBR&B hella intelligentsia. Taxidermy put a bird on it kickstarter
-							before they sold out cronut vice. Kogi twee hashtag, pickled
-							pitchfork fashion axe ugh.
-						</p>
-						<div className="project-image-parent">
-							<img src="https://vignette.wikia.nocookie.net/powerlisting/images/1/18/Yin-Yang.png/revision/latest?cb=20121028014051" />
+						<div className="project-content">
+							<p>
+								Lorem ipsum dolor amet truffaut mustache viral, vexillologist
+								PBR&B hella intelligentsia. Taxidermy put a bird on it
+								kickstarter before they sold out cronut vice. Kogi twee hashtag,
+								pickled pitchfork fashion axe ugh.
+							</p>
+							<div className="project-image-parent">
+								<img src="https://vignette.wikia.nocookie.net/powerlisting/images/1/18/Yin-Yang.png/revision/latest?cb=20121028014051" />
+							</div>
 						</div>
 						<div className="project-links">
 							<a href="#">GitHub&nbsp; &nbsp;</a>
