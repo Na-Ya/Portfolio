f1c3be70f11a0854f73b3ad9137dd148c1b1fb7b
Nabil Yafai <nabilyafai90@gmail.com>
2018-08-17 21:08:48 -0400

    added gif/image for projects

--- src/Projects.js
+++ src/Projects.js
@@ -8,26 +8,29 @@ const Projects = () => {
 			{projects.map((project, index) => {
 				return (
 					<div key={index}>
-						<h2>Project Title </h2>
+						<h2>RepliCode</h2>
 						<div className="project-content">
 							<div className="project-info">
-								<p>Role: </p>
-								<p>Tech Stack:</p>
+								<p>Role: Fullstack Software Engineer</p>
 								<p>
-									Lorem ipsum dolor amet truffaut mustache viral, vexillologist
-									PBR&B hella intelligentsia. Taxidermy put a bird on it
-									kickstarter before they sold out cronut vice. Kogi twee
-									hashtag, pickled pitchfork fashion axe ugh.
+									Tech Stack: React, Redux, AWS S3, Docker, Sequelize, Express,
+									Web Audio api.
+								</p>
+								<p>
+									Replicode is a content creation platform for creating
+									interactive coding tutorials.
 								</p>
 							</div>
 							<div className="project-image-parent">
-								<img src="https://vignette.wikia.nocookie.net/powerlisting/images/1/18/Yin-Yang.png/revision/latest?cb=20121028014051" />
+								<img src="https://i.imgur.com/LApgIyP.gif" />
 							</div>
 						</div>
 						<div className="project-links">
-							<a href="#">&nbsp;GitHub&nbsp; </a>
+							<a href="https://github.com/RepliCode/RepliCode">
+								&nbsp;GitHub&nbsp;{' '}
+							</a>
 							<h3>||</h3>
-							<a href="#">App Link</a>
+							<a href="https://replicode.herokuapp.com/">App Link</a>
 						</div>
 						{projects.length - 1 === index ? null : (
 							<hr className="project-break" />
