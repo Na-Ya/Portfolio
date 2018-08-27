4d2901cde584f8eda019917720199ea808feea82
Nabil Yafai <nabilyafai90@gmail.com>
2018-08-27 17:40:18 -0400

    added work experiences to relevant section

--- src/Experience.js
+++ src/Experience.js
@@ -93,28 +93,37 @@ class Experience extends Component {
 						<div className="flipper">
 							<div className="job-parent front">
 								<div className="job-info-parent">
-									<h3 id="job-title">Teaching Fellow</h3>
-									<p className="darker">Fullstack Academy of Code</p>
-									<p className="darker">07/2018 - Present</p>
+									<h3 id="job-title" className="center">
+										Admissions Coordinator
+									</h3>
+									<p className="darker">InterExchange</p>
+									<p className="darker">05/2014 - 03/2018</p>
 									<p>
-										Fullstack Academy is an immersive software engineering
-										coding bootcamp located in New York City and Chicago.
+										InterExchange manages cultural exchange programs for
+										participants from more than 60 countries and for Americans
+										who work and volunteer abroad.
 									</p>
 								</div>
 							</div>
 							<div className="job-parent back">
 								<div className="job-info-parent-back">
-									<h3 id="job-title">Teaching Fellow</h3>
+									<h3 id="job-title">Admissions Coordinator</h3>
 									<ul>
 										<li>
-											Daily mentorship of 28 students on engineering technical
-											concepts, algorithms, software architecture, debugging
-											strategies and writing clean code
-										</li>{' '}
+											Organized all incoming documents and payment information
+											for over 600 applicants per year.
+										</li>
 										<li>
-											Leading bi-weekly interactive review lectures covering
-											implementation of full-range of APIs and best practices of
-											Fullstack's NERDS stack
+											Created user guides, videos, and GIFs for users of our
+											application platform.
+										</li>
+										<li>
+											Assisted engineering team with testing new features for
+											the application platform.
+										</li>
+										<li>
+											Used GitHub to make edits to the InterExchange website and
+											publish new content.
 										</li>
 									</ul>
 								</div>
@@ -131,28 +140,33 @@ class Experience extends Component {
 						<div className="flipper">
 							<div className="job-parent front">
 								<div className="job-info-parent">
-									<h3 id="job-title">Teaching Fellow</h3>
-									<p className="darker">Fullstack Academy of Code</p>
-									<p className="darker">07/2018 - Present</p>
+									<h3 id="job-title" className="center">
+										Student Advocacy Assistant
+									</h3>
+									<p className="darker">NYU School of Engineering</p>
+									<p className="darker">10/2013 - 05/2014</p>
 									<p>
-										Fullstack Academy is an immersive software engineering
-										coding bootcamp located in New York City and Chicago.
+										The New York University Tandon School of Engineering is the
+										engineering and applied sciences school of New York
+										University.
 									</p>
 								</div>
 							</div>
 							<div className="job-parent back">
 								<div className="job-info-parent-back">
-									<h3 id="job-title">Teaching Fellow</h3>
+									<h3 id="job-title">Student Advocacy Assistant</h3>
 									<ul>
 										<li>
-											Daily mentorship of 28 students on engineering technical
-											concepts, algorithms, software architecture, debugging
-											strategies and writing clean code
+											Developed spreadsheets, forms, brochures, and posters for
+											the department.
 										</li>{' '}
 										<li>
-											Leading bi-weekly interactive review lectures covering
-											implementation of full-range of APIs and best practices of
-											Fullstack's NERDS stack
+											Created and organized files containing sensitive student
+											documentation.
+										</li>
+										<li>
+											Composed mass emails to students and professors on behalf
+											of the department.
 										</li>
 									</ul>
 								</div>
